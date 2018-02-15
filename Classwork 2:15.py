026e0f55d66dcc61767b28cc94ca0cd9ee8636f5
agordo25 <34726753+agordo25@users.noreply.github.com>
2018-02-15 14:44:40 -0500

    Add files via upload

---
+++ Classwork 2:15.py
@@ -0,0 +1,25 @@
+import random
+
+#generate two random numbers between 1 and 6 inclusive
+
+d1= random, randint(1,6)
+d2= random, randint(1,6)
+
+print(d1, d2)
+
+import turtle
+import random
+
+for i in range(2):
+    random_num1= random.radiant(1,7)
+    random_num2= random.radiant(1,7)
+    print(random_num1)
+    print(random_num2)
+
+def square(size):
+    for i in range(4):
+        turtle.forward(size)
+        turtle.right(90)
+
+for i in range(25):
+    square(random.radiant(10,100)
